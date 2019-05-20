a463d596b752b1f9511f3cb9ab20d463b4b8fcde
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-20 13:35:12 -0400

    Update space-invaders-game.py

--- space-invaders-game.py
+++ space-invaders-game.py
@@ -1,4 +1,4 @@
-from ggame import App, RectangleAsset, CircleAsset, LineAsset, ImageAsset, Frame, Sprite, LineStyle, Color
+from ggame import App, RectangleAsset, PolygonAsset, CircleAsset, LineAsset, ImageAsset, Frame, Sprite, LineStyle, Color
 import math
 import random
 
@@ -15,3 +15,59 @@ gray = Color(0x888888, 0.5)
 noline = LineStyle(0, black)
 whiteline = LineStyle(1, white)
 blackline = LineStyle(1, black)
+
+class Barriers(Sprite):
+    square = RectangleAsset(30, 30, noline, black)
+    
+    def __init__(self, position):
+        super().__init__(Barriers.square, position)
+        
+class Bullet(Sprite):
+    rect = RectangleAsset(20, 4, noline, black)
+    
+    def __init__(self, position, direction)
+        super().__init__(Bullet.rect, position)
+        self.speed = 5
+        self.vy = self.speed * direction
+        
+    def step(self):
+        self.y += self.vy
+        
+class Ship(Sprite):
+    ship = PolygonAsset((0,30), (15,0), (30,30), (15,15), noline, black)
+    
+    def __init__(self, position):
+        super().__init__(Ship.ship, position, width)
+        self.speed = 5
+        self.vx = 0
+        
+        SpaceInvadersGame.listenKeyEvent("keydown", "right arrow", moveRightOn)
+        SpaceInvadersGame.listenKeyEvent("keyup", "right arrow", moveRightOff)
+        SpaceInvadersGame.listenKeyEvent("keydown", "left arrow", moveLeftOn)
+        SpaceInvadersGame.listenKeyEvent("keydown", "left arrow", moveLeftOff)
+        
+    def shoot(self, event):
+        Bullet((self.x, self.y), -1)
+        
+    def moveRightOn(self, event):
+        self.vx = self.speed
+        
+    def moveRightOff(self, event):
+        self.vx = 0
+        
+    def moveLeftOn(self, event):
+        self.vx = -self.speed
+        
+    def moveLeftOff(self, event):
+        self.vx = 0
+        
+    def step(self):
+        self.x += self.vx
+        
+class SpaceInvadersGame(App):
+    def __init__(self):
+        super().__init__()
+        
+myapp = SpaceInvadersGame()
+myapp.run()
+        
