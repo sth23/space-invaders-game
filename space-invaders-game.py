526d4166cc6933556fc00baf807badc00c413591
sth23 <47009471+sth23@users.noreply.github.com>
2019-05-20 13:15:41 -0400

    Updated from Brython Server: 5/20/2019 1:15:41 PM

--- space-invaders-game.py
+++ space-invaders-game.py
@@ -1,1 +1,17 @@
+from ggame import App, RectangleAsset, CircleAsset, LineAsset, ImageAsset, Frame, Sprite, LineStyle, Color
+import math
+import random
 
+# Colors & lines
+red = Color(0xff0000, 1.0)
+orange = Color(0xffa500, 1.0)
+yellow = Color(0xffff00, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+purple = Color(0x800080, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+gray = Color(0x888888, 0.5)
+noline = LineStyle(0, black)
+whiteline = LineStyle(1, white)
+blackline = LineStyle(1, black)
